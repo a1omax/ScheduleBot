a987b5ff1cc627714f4069809a7f17b0696bc34e
a1omax <53946133+a1omax@users.noreply.github.com>
2021-02-13 05:28:02 +0200

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -7,8 +7,9 @@ from arg import *
 bot = telebot.TeleBot(cfg.TOKEN)
 
 
-def wC(arg):
+def day_change(arg):
     return arg + count
+
 
 def hours():
     hour = int(now.strftime("%H"))
@@ -28,115 +29,136 @@ def hours():
     else:
         return -1
 
-def weekNow(change=0):
 
-    week = (int(now.strftime("%V"))+change)%2
-    #Учебный год начинаться с четной недели (не совпадает)
+def week_now(change=0):
+    week = (int(now.strftime("%V")) + change) % 2
+    # Учебный год начинаться с четной недели (не совпадает)
     if week == 1:
         return schedule[1]
     elif week == 0:
         return schedule[0]
 
-def numberOfPara(arg):
-    return (weekNow().get(now.weekday())).get(arg)
 
-def paraTodayByArg(key=1):
+def number_of_para(arg):
+    return (week_now().get(now.weekday())).get(arg)
 
-    para = numberOfPara(key)
-    if para != None:
-        return "\nУ первой подгруппы: " + para[0]+ "\nУ второй подгруппы: " + para[1]
+
+def para_today_by_arg(key=1):
+    para = number_of_para(key)
+    if para is not None:
+        return "\nУ первой подгруппы: " + para[0] + "\nУ второй подгруппы: " + para[1]
     else:
         return "\nУ первой и второй подгруппы сейчас нет пар"
 
-def paraByKeyWord(day): 
-    if day < now.weekday() and day >= 0:
-        anotherWeek = 1
-    else:
-        day = wC(day)
-        anotherWeek = day // 7
 
-    def output(i,day=0):
+def para_by_key_word(day):
+    if now.weekday() > day and day >= 0:
+        another_week = 1
+    else:
+        day = day_change(day)
+        another_week = day // 7
+
+    def output(i, day = 0):
         return str(i) + " пара: " + "\n" + \
-        (weekNow(anotherWeek).get(day%7)).get(i)[0] + "\n" + \
-        (weekNow(anotherWeek).get(day%7)).get(i)[1] + "\n"
+               (week_now(another_week).get(day % 7)).get(i)[0] + "\n" + \
+               (week_now(another_week).get(day % 7)).get(i)[1] + "\n"
+
     out = "\n"
-    for i in range(1,6):
-        out+=output(i,day)
+    for i in range(1, 6):
+        out += output(i, day)
     return out
 
 
-@bot.message_handler(commands=['para','пара'])
-def start_message(message):
+chatId = None
+
+
+def listener(message):
     global now
     now = datetime.now()
 
-    def theDay(value):
-        return (now.weekday() + value)%7
+    msg_txt = ""
+    chat_id = ""
 
-    chatId = message.chat.id
-    keyW = message.text
+    for m in message:
+        chat_id = m.chat.id
+        if m.content_type == 'text':
+            msg_txt = m.text.lower()
+
+    def the_day(value):
+        return (now.weekday() + value) % 7
+
+    splited_msg = msg_txt.splitlines()
+
     global count
-    count = keyW.count('после') - keyW.count('поза')  - keyW.count('перед')  - keyW.count('до')
+    for i in para_input:
+        for j in splited_msg:
+            if i == j:
+                msg_txt = msg_txt.replace(i, '')
+                count = msg_txt.count('после') - msg_txt.count('поза') - msg_txt.count('перед') - msg_txt.count('до')
 
-    keyW = keyW.replace('поза','')
-    keyW = keyW.replace('перед', '')
-    keyW = keyW.replace('после', '')
-    keyW = keyW.replace('до', '')
-    keyW = (keyW.strip()).split()
-    print (keyW)
+                msg_txt = msg_txt.replace('поза', '')
+                msg_txt = msg_txt.replace('перед', '')
+                msg_txt = msg_txt.replace('после', '')
+                msg_txt = msg_txt.replace('до', '')
+                msg_txt = (msg_txt.strip()).split()
 
-    day = paraTodayByArg(hours()) #по умолчанию
+                print(msg_txt)
 
-    for keyWord in keyW:
-        if keyWord in today:
-            print("today")
-            day = paraByKeyWord(theDay(0))
-            break
-        elif keyWord in tomorrow:
-            print("tomorrow")
-            day = paraByKeyWord(theDay(1))
-            break
-        elif keyWord in yesterday:
-            print("yesterday")
-            day = paraByKeyWord(theDay(-1))
-            break
-        elif keyWord in monday:
-            print("mon")
-            day = paraByKeyWord(0)
-            break
-        elif keyWord in tuesday:
-            print("tuesday")
-            day = paraByKeyWord(1)
-            break
-        elif keyWord in wednesday:
-            day = paraByKeyWord(2)
-            print("wednesday")
-            break
-        elif keyWord in thursday:
-            day = paraByKeyWord(3)
-            print("thursday")
-            break
-        elif keyWord in friday:
-            print("friday")
-            day = paraByKeyWord(4)
-            break
-        elif keyWord in saturday:
-            print("saturday")
-            day = paraByKeyWord(5)
-            break
-        elif keyWord in sunday:
-            print("sunday")
-            day = paraByKeyWord(6)
-            break
-        elif keyWord in next:
-            print("next")
-            day = paraTodayByArg(hours()+1)
-            break
-        elif keyWord in number:
-            print("number")
-            day = paraTodayByArg(int(keyWord))
-            break
+                day = para_today_by_arg(hours())  # по умолчанию
 
-    bot.send_message(chatId, str(day))
+                for keyWord in msg_txt:
+                    if keyWord in today:
+                        print("today")
+                        day = para_by_key_word(the_day(0))
+                        break
+                    elif keyWord in tomorrow:
+                        print("tomorrow")
+                        day = para_by_key_word(the_day(1))
+                        break
+                    elif keyWord in yesterday:
+                        print("yesterday")
+                        day = para_by_key_word(the_day(-1))
+                        break
+                    elif keyWord in monday:
+                        print("mon")
+                        day = para_by_key_word(0)
+                        break
+                    elif keyWord in tuesday:
+                        print("tuesday")
+                        day = para_by_key_word(1)
+                        break
+                    elif keyWord in wednesday:
+                        day = para_by_key_word(2)
+                        print("wednesday")
+                        break
+                    elif keyWord in thursday:
+                        day = para_by_key_word(3)
+                        print("thursday")
+                        break
+                    elif keyWord in friday:
+                        print("friday")
+                        day = para_by_key_word(4)
+                        break
+                    elif keyWord in saturday:
+                        print("saturday")
+                        day = para_by_key_word(5)
+                        break
+                    elif keyWord in sunday:
+                        print("sunday")
+                        day = para_by_key_word(6)
+                        break
+                    elif keyWord in next:
+                        print("next")
+                        day = para_today_by_arg(hours() + 1)
+                        break
+                    elif keyWord in number:
+                        print("number")
+                        day = para_today_by_arg(int(keyWord))
+                        break
 
-bot.polling()
+                bot.send_message(chat_id, str(day))
+                break
+
+
+bot.set_update_listener(listener)
+bot.polling(none_stop=True)
